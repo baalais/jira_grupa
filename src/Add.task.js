f1630b0020f4d96f0d6dc47674047d0ce0768c80
demo212121 <113087144+demo212121@users.noreply.github.com>
2023-11-13 11:28:05 +0200

    saku veidot add.task

---
+++ src/Add.task.js
@@ -0,0 +1,14 @@
+import './Add.task.css';
+
+function AddTask() {
+        return (
+            <div class = "container" >
+              <h1>Things to do...</h1>
+              <h2>Description...</h2>
+              <h3>Date</h3>
+              <button class = "addTaskButton" >Add task</button>
+            </div>
+         );
+};  
+  
+  export default AddTask;
