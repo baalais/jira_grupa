2e6d80642acc7072101dfb30189bb68e5bdabed8
Sintijaa <ipb21.s.martinsone@vtdt.edu.lv>
2023-11-13 11:37:01 +0200

    shit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './style/Main.css';
 export default function App() {
     return (
         <div className='flex flex-col items-center container mx-auto max-w-2xl shadow '>
-            <span className='text-xl '>JIRA like todo list</span>
+            <span className='text-xl white '>JIRA like todo list</span>
         </div>
     )
 }
