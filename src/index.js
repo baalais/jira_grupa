3fa7e9941ca094317d3036d307e91cd637edae15
kkarliskk <karlisbirk@gmail.com>
2023-11-13 13:55:06 +0200

    asd

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import { Router, Route, Routes, Link } from 'react-router-dom';
 import App from './App';
+import Login from './Routes/Login';
+import Register from './Routes/Register';
+import { MantineProvider } from '@mantine/core';
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route path="dashboard" element={<Dashboard />} />
+        {/* ... etc. */}
+      </Route>
+    )
+  );
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode> 
+      <HashRouter>
+         <Routes>
+            <Route index element={<App />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+         </Routes>
+      </HashRouter>
+
 );
