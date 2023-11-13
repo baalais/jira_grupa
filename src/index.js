9a498c57742b22b32f3fcfcffc39da6d7b58e76f
baalais <tomassrudzitis2014@gmail.com>
2023-11-13 13:47:59 +0200

    Addtask frontend pabeigts

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Addtask from './Routes/Add-task.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Addtask />
     </React.StrictMode> 
 );
