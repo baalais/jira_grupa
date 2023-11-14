e9acc49531f5f455a36a45c8c3c1abd50038642d
dsa <99286765+baalais@users.noreply.github.com>
2023-11-14 12:49:14 +0200

    asd

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './App';
-import Addtask from './Routes/Add-task.js'
+import Login from './Routes/Login';
+import Register from './Routes/Register';
+import AddTask from './Routes/Add-task';
+
+export default function Index() {
+  return (
+    <BrowserRouter>
+      <Routes>
+          <Route index element={<App />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/Add-task" element={<AddTask/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-        <Addtask />
-    </React.StrictMode> 
-);
+root.render(<Index />);
