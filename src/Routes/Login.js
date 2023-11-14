39d694b2e64f1e2be3b800e3dc7c3060a0206ede
kkarliskk <karlisbirk@gmail.com>
2023-11-14 13:52:30 +0200

    sign in/ sign up frontend

--- src/Routes/Login.js
+++ src/Routes/Login.js
@@ -14,10 +14,28 @@ export default function Login(){
     
     return (
         <>
-            <div className='border 1px black'> Hello World! </div>
-            <form name='login_form' id='login_form'>
-                <input type='text'></input>
-            </form>
+        <div className='background-secondary'></div>
+        <div className='background-primary'></div>
+            <div className='register-container'>
+                <div className='register-box'>
+                    <div className='split-left'></div>
+                    <div className='split-right'>
+                        <div className='form-header'>
+                            <h1>Sign In</h1>
+                        </div>
+                        <form method='post' name='register_form' id='register_form'>
+                            <input type='text' placeholder='Username'></input>
+                            <input type='text' placeholder='Email'></input>
+                            <input type='password' placeholder='Password'></input>
+                            <input type='password' placeholder='Repeat password'></input>
+                            <button>Sign In</button>
+                        </form>
+                        <div className='form-footer'>
+                            <p>Already have an account?<a href="#">Sign In!</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
