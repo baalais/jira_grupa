a8c4c76b39e9f02fabd0da03e0a0f7d19a2325e1
kkarliskk <karlisbirk@gmail.com>
2023-11-21 09:34:07 +0200

    sum changerinos

--- api/db.php
+++ api/db.php
@@ -1,32 +1,52 @@
 <?php
-header('Access-Control-Allow-Origin: http://localhost:3000');
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type');
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $title = $_POST['title'];
-    $description = $_POST['description'];
-    $due_date = $_POST['date'];
+include './headers.php';
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "jira_grupa";
+$sql = new DB($GLOBALS['host'], $GLOBALS['user'], $GLOBALS['pass'], $GLOBALS['db']);
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+class DB {
+  private $servername = "localhost";
+  private $username = "root";
+  private $password = "";
+  private $dbname = "jira_grupa";
+  private $conn;
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+  // Constructor to establish the database connection
+  public function __construct() {
+      $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+
+      // Check connection
+      if ($this->conn->connect_error) {
+          die("Connection failed: " . $this->conn->connect_error);
+      }
+      echo "Connected successfully";
+  }
 }
-echo "Connected successfully";
 
-$sql = "INSERT INTO `tasks`(`user_id`, `title`, `description`, `due_date`) VALUES ('user_id','title','description','dueDate')";
+// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+//     $title = $_POST['title'];
+//     $description = $_POST['description'];
+//     $due_date = $_POST['date'];
+
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "jira_grupa";
+
+// // Create connection
+// $conn = new mysqli($servername, $username, $password, $dbname);
+
+// // Check connection
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
+// echo "Connected successfully";
+
+// $sql = "INSERT INTO `tasks`(`user_id`, `title`, `description`, `due_date`) VALUES ('user_id','title','description','dueDate')";
 
 
    
-}else{
-    echo "nav";
-}
+// }else{
+//     echo "nav";
+// }
 
 ?>
