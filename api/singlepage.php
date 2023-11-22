f8a78abae73c00fea486756ee0be28f932f7f812
JustsPuspurs <144904136+JustsPuspurs@users.noreply.github.com>
2023-11-22 15:23:13 +0200

    aasddd

--- api/singlepage.php
+++ api/singlepage.php
@@ -1,25 +1,50 @@
 <?php
-include_once('db.php');
+include './headers.php';
+include './db.php';
 
-// public function getTaskById($id)
-//     {
-//         $stmt = $this->db->prepare('SELECT * FROM tasks WHERE id = ?');
-//         $stmt->execute([$id]);
-//         $task = $stmt->fetch(PDO::FETCH_ASSOC);
-//         return $task;
-//     }
+class Single {
+    private $conn;
+
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+    public function getTaskById($id) {
+        $stmt = $this->conn->prepare('SELECT tasks.*, users.username FROM tasks 
+                                     INNER JOIN users ON tasks.user_id = users.id 
+                                     WHERE tasks.id = ?');
+        $stmt->bind_param('i', $id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $task = $result->fetch_assoc();
+        $stmt->close();
+        return $task;
+    }
+}
+
+// Instantiate the DB class
+$sql = new DB();
+
+// Instantiate the Single class and pass the database connection
+$api = new Single($sql->getConnection());
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     if (isset($_GET['id'])) {
         $id = $_GET['id'];
+
         $task = $api->getTaskById($id);
+
         if ($task) {
+            // Set proper headers for a JSON response
+            header('Content-Type: application/json');
+            echo json_encode($task);
         } else {
             http_response_code(404);
             echo json_encode(['message' => 'Task not found']);
         }
+    } else {
+        http_response_code(400);
+        echo json_encode(['message' => 'No task ID provided']);
     }
-    
 }
-// 'SELECT * FROM tasks WHERE id = ?'
 ?>
--- src/Routes/SinglePage.js
+++ src/Routes/SinglePage.js
@@ -1,23 +1,45 @@
 import '../Style/SinglePage.css';
+import React, { useEffect, useState } from 'react';
 
+function SinglePage() {
+    const [task, setTask] = useState(null);
+    const defaultTaskId = 1; // Set your default task ID here
 
-function SinglePage(){
-    return(
-        <html>
-            <div class="box">
-                <div class="top">
-                    <h1>Task Name</h1>
-                </div>
-                <div class = "desc">
-                    <p>Description:</p>
-                </div>
-                <div class = "bottom">
-                    <p>Date:</p>
-                    <p>IN PROGRESS</p>
-                </div>
+    useEffect(() => {
+        // For testing purposes, use the defaultTaskId instead of getting it from the URL
+        const taskId = defaultTaskId;
+
+        // Make an AJAX request to fetch task details
+        fetch(`../api/singlepage.php?id=${taskId}`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => {
+                setTask(data);
+            })
+            .catch(error => {
+                console.error('Error fetching task:', error.message); // Log the detailed error message
+            });
+    }, []);
+
+    return (
+        <div className="box">
+            <div className="top">
+                <h1>{task ? task.title : 'Loading...'}</h1>
             </div>
-
-        </html>
-    )
+            <div className="desc">
+                <p>Description: {task ? task.description : 'Loading...'}</p>
+            </div>
+            <div className="bottom">
+                <p>Date: {task ? task.due_date : 'Loading...'}</p>
+                <p>Status: {task ? task.status : 'Loading...'}</p>
+                <p>User: {task ? task.username : 'Loading...'}</p>
+            </div>
+        </div>
+    );
 }
+
 export default SinglePage;
