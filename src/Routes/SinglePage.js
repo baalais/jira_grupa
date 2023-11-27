1facea86029a4eed470c94ee662633387120f56c
JustsPuspurs <144904136+JustsPuspurs@users.noreply.github.com>
2023-11-27 15:25:15 +0200

    asd

--- src/Routes/SinglePage.js
+++ src/Routes/SinglePage.js
@@ -1,45 +1,37 @@
-import '../Style/SinglePage.css';
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-function SinglePage() {
-    const [task, setTask] = useState(null);
-    const defaultTaskId = 1; // Set your default task ID here
+const SinglePage = () => {
+  const [data, setData] = useState(null);
 
-    useEffect(() => {
-        // For testing purposes, use the defaultTaskId instead of getting it from the URL
-        const taskId = defaultTaskId;
-
-        // Make an AJAX request to fetch task details
-        fetch(`../api/singlepage.php?id=${taskId}`)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`HTTP error! Status: ${response.status}`);
-                }
-                return response.json();
-            })
-            .then(data => {
-                setTask(data);
-            })
-            .catch(error => {
-                console.error('Error fetching task:', error.message); // Log the detailed error message
-            });
-    }, []);
-
-    return (
-        <div className="box">
-            <div className="top">
-                <h1>{task ? task.title : 'Loading...'}</h1>
-            </div>
-            <div className="desc">
-                <p>Description: {task ? task.description : 'Loading...'}</p>
-            </div>
-            <div className="bottom">
-                <p>Date: {task ? task.due_date : 'Loading...'}</p>
-                <p>Status: {task ? task.status : 'Loading...'}</p>
-                <p>User: {task ? task.username : 'Loading...'}</p>
-            </div>
-        </div>
-    );
+  const fetchData = async () => {
+    try {
+        const response = await fetch('singlepage.php?id=1');
+        const json = await response.json();
+        setData(json);
+      } catch (error) {
+        console.error(error);
+      }
+      
 }
+
+  
+  useEffect(() => {
+    fetchData();
+  }, []);
+  
+
+  return (
+    <div>
+      {data ? (
+        <div>
+          <h1>{data.title}</h1>
+          <p>{data.body}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
 
 export default SinglePage;
