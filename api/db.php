b625d258b99c1fe5623c8b1e7afeea8e34e67226
kkarliskk <karlisbirk@gmail.com>
2023-11-27 15:22:38 +0200

    POST data works for register

--- api/db.php
+++ api/db.php
@@ -5,52 +5,6 @@ include './var.php';
 
 $sql = new MysqliDb($GLOBALS['host'], $GLOBALS['user'], $GLOBALS['pass'], $GLOBALS['db']);
 
-// class DB {
-//   private $servername = "localhost";
-//   private $username = "root";
-//   private $password = "";
-//   private $dbname = "jira_grupa";
-//   private $conn;
-
-
-//   public function __construct() {
-//       $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
-
-
-//       if ($this->conn->connect_error) {
-//           die("Connection failed: " . $this->conn->connect_error);
-//       }
-//       echo "Connected successfully";
-//   }
-// }
-
-// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-//     $title = $_POST['title'];
-//     $description = $_POST['description'];
-//     $due_date = $_POST['date'];
-
-// $servername = "localhost";
-// $username = "root";
-// $password = "";
-// $dbname = "jira_grupa";
-
-// // Create connection
-// $conn = new mysqli($servername, $username, $password, $dbname);
-
-// // Check connection
-// if ($conn->connect_error) {
-//   die("Connection failed: " . $conn->connect_error);
-// }
-// echo "Connected successfully";
-
-// $sql = "INSERT INTO `tasks`(`user_id`, `title`, `description`, `due_date`) VALUES ('user_id','title','description','dueDate')";
-
-
-   
-// }else{
-//     echo "nav";
-// }
-
 class MysqliDb
 {
 
