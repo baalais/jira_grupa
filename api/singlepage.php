696ad148ed20c04d5345e46eace4971964a26f6c
JustsPuspurs <144904136+JustsPuspurs@users.noreply.github.com>
2023-11-29 15:23:20 +0200

    neiet

--- api/singlepage.php
+++ api/singlepage.php
@@ -1,4 +1,5 @@
 <?php
+// Establish database connection
 $servername = "localhost";
 $username = "root"; // Replace with your MySQL username
 $password = ""; // Replace with your MySQL password
@@ -9,16 +10,22 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die(json_encode(['error' => 'Connection failed: ' . $conn->connect_error]));
 }
 
+// Handling the GET request
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     if (isset($_GET['id'])) {
         $id = $_GET['id'];
+    } else {
+        // If no ID is provided, fetch the first task
+        $id = 1;
+    }
 
+    try {
         $stmt = $conn->prepare('SELECT tasks.*, users.username FROM tasks 
-                                     INNER JOIN users ON tasks.user_id = users.id 
-                                     WHERE tasks.id = ?');
+                                    INNER JOIN users ON tasks.user_id = users.id 
+                                    WHERE tasks.id = ?');
         $stmt->bind_param('s', $id);
         $stmt->execute();
         $result = $stmt->get_result();
@@ -26,16 +33,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         $stmt->close();
 
         if ($task) {
-            // Set proper headers for a JSON response
             header('Content-Type: application/json');
             echo json_encode($task);
         } else {
             http_response_code(404);
             echo json_encode(['message' => 'Task not found']);
         }
-    } else {
-        http_response_code(400);
-        echo json_encode(['message' => 'No task ID provided']);
+    } catch (Exception $e) {
+        http_response_code(500);
+        echo json_encode(['error' => $e->getMessage()]);
     }
 }
 
--- src/Routes/SinglePage.js
+++ src/Routes/SinglePage.js
@@ -1,34 +1,46 @@
 import React, { useState, useEffect } from 'react';
 
-const SinglePage = () => {
-  const [data, setData] = useState(null);
+const SinglePage = ({ taskId }) => {
+  const [task, setTask] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  const fetchData = async () => {
-    try {
-        const response = await fetch('singlepage.php?id=1');
-        const json = await response.json();
-        setData(json);
-      } catch (error) {
-        console.error(error);
-      }
-      
-}
-
-  
   useEffect(() => {
-    fetchData();
-  }, []);
-  
+    const fetchTask = async () => {
+      try {
+        const response = await fetch(`http://localhost:3000/api/singlepage.php?id=${taskId}`);
+        const data = await response.json();
+
+        if (!response.ok) {
+          throw new Error(data.message || 'Failed to fetch task');
+        }
+
+        setTask(data);
+        setError(null);
+      } catch (error) {
+        setError(`Failed to fetch task: ${error.message}`);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchTask();
+  }, [taskId]);
 
   return (
-    <div>
-      {data ? (
+    <div className="container">
+      <h1>Task Details</h1>
+      {loading && <p>Loading...</p>}
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      {task && (
         <div>
-          <h1>{data.title}</h1>
-          <p>{data.body}</p>
+          <p>ID: {task.id}</p>
+          <p>Title: {task.title}</p>
+          <p>Description: {task.description}</p>
+          <p>Due Date: {task.due_date}</p>
+          <p>User ID: {task.user_id}</p>
+          <p>Status: {task.status}</p>
         </div>
-      ) : (
-        <p>Loading...</p>
       )}
     </div>
   );
