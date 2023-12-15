d9d4e4c05f0687ad8c09dd16127704d1f92ba86a
JustsPuspurs <144904136+JustsPuspurs@users.noreply.github.com>
2023-12-15 09:49:42 +0200

    viss darbojas iznemot mobile view

--- src/Routes/SinglePage.js
+++ src/Routes/SinglePage.js
@@ -1,45 +1,50 @@
 import React, { useState, useEffect } from 'react';
 import '../Style/SinglePage.css';
 
-// Define the TaskDetails component
 const SinglePage = () => {
     const [task, setTask] = useState(null);
     const [error, setError] = useState(null);
+    const [boxHeight, setBoxHeight] = useState(300); // Set an initial height
 
-    // Fetch task data when the component mounts
     useEffect(() => {
-        const fetchTask = async () => {
-            try {
-                const response = await fetch('http://localhost/justs/jira_grupa/api/singlepage.php');
-                const data = await response.json();
+      const fetchTask = async () => {
+          try {
+              const response = await fetch('http://localhost/justsipb21/Jauna%20mape/api/singlepage.php');
+              const data = await response.json();
+  
+              if (response.ok) {
+                  setTask(data);
+  
+                  // Set a maximum height for the box
+                  const maxHeight = 500; // Adjust the maximum height as needed
+                  setBoxHeight(maxHeight);
+              } else {
+                  setError(`Error: ${data.error || 'Unknown error'}`);
+              }
+          } catch (error) {
+              setError(`Error: ${error.message}`);
+          }
+      };
+  
+      fetchTask();
+  }, []);
+  
 
-                if (response.ok) {
-                    setTask(data);
-                } else {
-                    setError(`Error: ${data.error || 'Unknown error'}`);
-                }
-            } catch (error) {
-                setError(`Error: ${error.message}`);
-            }
-        };
-
-        fetchTask();
-    }, []);
-
-    // Render the component with dynamic data
     return (
-        <div className="box">
-            <div className="top">
-                <h1>{task?.title}</h1>
+        <div className="container">
+            <div className="box" style={{ height: `${boxHeight}px` }}>
+                <div className="top">
+                    <h1>{task?.title}</h1>
+                </div>
+                <div className="desc">
+                    <p>{task?.description}</p>
+                </div>
+                <div className="bottom">
+                    <p>Due Date: {task?.due_date}</p>
+                    <p>{task?.status}</p>
+                </div>
+                {error && <p style={{ color: 'red' }}>{error}</p>}
             </div>
-            <div className="desc">
-                <p>{task?.description}</p>
-            </div>
-            <div className="bottom">
-                <p>Due Date: {task?.due_date}</p>
-                <p>Status: {task?.status}</p>
-            </div>
-            {error && <p style={{ color: 'red' }}>{error}</p>}
         </div>
     );
 };
